e6c6da061220e030b71780bfb2a0375d9b6a81b1
alireza <gtsd2233@gmail.com>
2016-08-08 07:07:58 +0000

    Update Supergroup.lua

--- plugins/Supergroup.lua
+++ plugins/Supergroup.lua
@@ -1033,7 +1033,7 @@ function show_supergroup_settingsmod(msg, target)
   local gp_type = data[tostring(msg.to.id)]['group_type']
   
   local settings = data[tostring(target)]['settings']
-  local text = "\n⚙SuperGroup settings⚙:\n____________________\n⚡️Lock links : "..settings.lock_link.."\n⚡️Lock contacts: "..settings.lock_contacts.."\n⚡️Lock flood: "..settings.flood.."\n⚡️Flood sensitivity : "..NUM_MSG_MAX.."\n⚡️Lock spam: "..settings.lock_spam.."\n⚡️Lock Arabic: "..settings.lock_arabic.."\n⚡️Lock Member: "..settings.lock_member.."\n⚡️Lock RTL: "..settings.lock_rtl.."\n⚡️Lock Tgservice: "..settings.lock_tgservice.."\n⚡️Lock sticker: "..settings.lock_sticker.."\n⚡️Lock tag: "..settings.tag.."\n⚡️Lock emoji: "..settings.emoji.."\n⚡️Lock english: "..settings.english.."\n⚡️Lock Forward: "..settings.fwd.."\n⚡️Lock reply: "..settings.reply.."\n⚡️Lock join: "..settings.join.."\n⚡️Lock username: "..settings.username.."\n⚡️Lock media: "..settings.media.."\n⚡️Lock fosh: "..settings.fosh.."\n⚡️Lock leave: "..settings.leave.."\n⚡️Lock bots: "..bots_protection.."\n⚡️Lock operator: "..settings.operator.."\n____________________\n⚙Easy Sweet&Faster Switch⚙:️\n____________________\n⚡️ Switch Model Etehad: "..settings.etehad.."\n⚡️Lock all: "..settings.all.."\n____________________\nℹ️About Groupℹ️:️\n____________________\n⚡️ Group type: "..gp_type.."\n⚡️Public: "..settings.public.."\n⚡️Strict settings: "..settings.strict.."\n____________________\n💥Bot Version : 1.0 💥\n💥Sinal Bot💥\n@SignalTM"
+  local text = "⚙SuperGroup settings⚙\n🔐Lock links: "..settings.lock_link.."\n🔐Lock contacts: "..settings.lock_contacts.."\n🔐Lock flood: "..settings.flood.."\n🔐Flood sensitivity: "..NUM_MSG_MAX.."\n🔐Lock spam: "..settings.lock_spam.."\n🔐Lock Arabic: "..settings.lock_arabic.."\n🔐Lock Member: "..settings.lock_member.."\n🔐Lock RTL: "..settings.lock_rtl.."\n🔐Lock Tgservice: "..settings.lock_tgservice.."\n🔐Lock sticker: "..settings.lock_sticker.."\n🔐Lock tag(#): "..settings.tag.."\n🔐Lock emoji: "..settings.emoji.."\n🔐Lock english: "..settings.english.."\n🔐Lock fwd(forward): "..settings.fwd.."\n🔐Lock reply:  "..settings.reply.."\n🔐Lock join: "..settings.join.."\n🔐Lock username(@): "..settings.username.."\n🔐Lock media: "..settings.media.."\n🔐Lock fosh: "..settings.fosh.."\n🔐Lock leave: "..settings.leave.."\n🔐Lock bots: "..bots_protection.."\n🔐Lock operator: "..settings.operator.."\n____________________\n🔷Easy Sweet&Faster Switch🔷\n🖲Switch Model Etehad: "..settings.etehad.."\n🔐Lock all: "..settings.all.."\n____________________\nℹ️About Groupℹ️\n____________________\n🖲group type: "..gp_type.."\n💢Public: "..settings.public.."\n🔷Strict settings : "..settings.strict.."\n____________________\n💢bot version : v2.9💢\n👑UBUNTU👑\nℹ️ @UBUNTU_TEAM ℹ️"
   return text
 end
 
@@ -2628,7 +2628,7 @@ local function run(msg, matches)
 		end
 
 		if matches[1] == 'help' and not is_owner(msg) then
-			text = "Message /superhelp to @SignalTG in private for SuperGroup help"
+			text = "Message /superhelp to @UBUNTU_TG in private for SuperGroup help"
 			reply_msg(msg.id, text, ok_cb, false)
 		elseif matches[1] == 'help' and is_owner(msg) then
 			local name_log = user_print_name(msg.from)
